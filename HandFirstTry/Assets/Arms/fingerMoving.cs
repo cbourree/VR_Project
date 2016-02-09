03ba846e1dd2c87465b7da2c5d56fa91f6e0a272
Pomerleau Sébastien <charles-bourree@hotmail.fr>
2016-02-09 14:26:39 -0500

    Ajout bon Unity

---
+++ HandFirstTry/Assets/Arms/fingerMoving.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fingerMoving : MonoBehaviour {
+
+
+    public int vitesseDoigts = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(0, 0, vitesseDoigts); 
+	}
+}
