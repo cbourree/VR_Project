d51671385633a88c1fcdccd895ff7b1bdd257fe1
cbourree <cbourree@hotmail.fr>
2016-02-09 12:35:35 -0500

    Ajout moyenne glissante sur le gyro

--- Code_Arduino/test.ino/test.ino.ino
+++ Code_Arduino/test.ino/test.ino.ino
@@ -1,46 +1,40 @@
-#include "I2Cdev.h"
 #include "MPU6050.h"
 #include <SPI.h>                         // Include the SPI library
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-    #include "Wire.h"
-#endif
+#include "Wire.h"
 
 SPISettings MCP3008(2000000, MSBFIRST, SPI_MODE0);
 MPU6050 accelgyro(0x69); // <-- use for AD0 high
 
-int16_t ax, ay, az;
-int16_t gx, gy, gz;
+//int16_t ax, ay, az;
+//int16_t gx, gy, gz;
 
 const int  CS_MCP3008       = 4;          // ADC Chip Select
 
-const byte adc_single_ch3   = (0x0B);     // ADC Channel 3
-const byte adc_single_ch4   = (0x0C);     // ADC Channel 4
-const byte adc_single_ch5   = (0x0D);     // ADC Channel 5
-const byte adc_single_ch6   = (0x0E);     // ADC Channel 6
-const byte adc_single_ch7   = (0x0F);     // ADC Channel 7
+const byte adc_single_ch3 = 0x0B;     // ADC Channel 3
+const byte adc_single_ch4 = 0x0C;     // ADC Channel 4
+const byte adc_single_ch5 = 0x0D;     // ADC Channel 5
+const byte adc_single_ch6 = 0x0E;     // ADC Channel 6
+const byte adc_single_ch7 = 0x0F;     // ADC Channel 7
 
+int16_t G_doigts[18];
 
 void setup() {
-    delay(2000);
+    for (int i = 0 ; i < 18 ; i++) {
+      G_doigts[i] = 0;
+    }
+    delay(1000);
     Serial.begin(9600);
     SPI.begin();
+    Wire.begin();
     
     pinMode       (CS_MCP3008, OUTPUT);
     digitalWrite  (CS_MCP3008, LOW);        // Cycle the ADC CS pin as per datasheet
     digitalWrite  (CS_MCP3008, HIGH);
-    
-    // join I2C bus (I2Cdev library doesn't do this automatically)
-    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-        Wire.begin();
-    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-        Fastwire::setup(400, true);
-    #endif
 
     Wire.beginTransmission(0x69);
     accelgyro.initialize();
 
     //Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
-
 }
 
 void loop() {
@@ -63,18 +57,21 @@ void loop() {
   Serial.write       (";D");
   Serial.print     ((adc_reading * 5.0) / 1024) * 2;
 
-  // read raw accel/gyro measurements from device
-    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    for (int i = 0 ; i < 3 ; i++) {
+      accelgyro.getMotion6(&(G_doigts[i * 6]), &(G_doigts[(i * 6) + 1]), &(G_doigts[(i * 6) + 2]), &(G_doigts[(i * 6) + 3]), &(G_doigts[(i * 6) + 4]), &(G_doigts[(i * 6) + 5]));
+      delay(10);
+    }
+    
     Serial.write(";AX");
-    printDouble(ax);Serial.write(";AY:");
-    printDouble(ay);Serial.write(";AZ:");
-    printDouble(az);Serial.write(";GX:");
-    printDouble(gx);Serial.write(";GY:");
-    printDouble(gy);Serial.write(";GZ:");
-    printDouble(gz);Serial.write("F\n");
+    printDouble((G_doigts[0] + G_doigts[6] + G_doigts[12]) / 3);Serial.write(";AY:");
+    printDouble((G_doigts[1] + G_doigts[7] + G_doigts[13]) / 3);Serial.write(";AZ:");
+    printDouble((G_doigts[2] + G_doigts[8] + G_doigts[14]) / 3);Serial.write(";GX:");
+    printDouble((G_doigts[3] + G_doigts[9] + G_doigts[15]) / 3);Serial.write(";GY:");
+    printDouble((G_doigts[4] + G_doigts[10] + G_doigts[16]) / 3);Serial.write(";GZ:");
+    printDouble((G_doigts[5] + G_doigts[11] + G_doigts[17]) / 3);Serial.write("F\n");
 
     // blink LED to indicate activity
-    delay(40);
+    delay(20);
 }
 
 int adc_single_channel_read(byte readAddress)
